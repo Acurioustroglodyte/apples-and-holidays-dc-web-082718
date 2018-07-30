81a10fd008523264252baba84aff2e2f632df9af
Trevor Jameson <tjameson15@gmail.com>
2018-07-30 00:30:23 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,8 +78,8 @@ def all_supplies_in_holidays(holiday_hash)
         word.strip!
         word.capitalize!
       end
-      output = output.join
-
+      output = output.join(" ")
+      puts output
     end
   end
 end
