15d3ecab34d09afb9639ead14db225f1600a75cc
Trevor Jameson <tjameson15@gmail.com>
2018-07-30 00:14:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,6 +74,9 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |day, items|
       output = " #{day.to_s.match(/[\D]+/)}:"
       output = output.split("_")
+      output.each do |word|
+        word.capitalize
+      end
       puts output
       puts "#{output}, items"
     end
