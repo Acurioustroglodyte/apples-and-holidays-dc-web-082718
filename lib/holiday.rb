0184d2b6ab9816aabc1bba0a9615aed59ce72bfa
Trevor Jameson <tjameson15@gmail.com>
2018-07-30 00:41:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def all_supplies_in_holidays(holiday_hash)
       items_output = items.join(", ")
 
       # Print output *high fives*
-      puts " #{holiday_output} #{items_output}"
+      puts "  #{holiday_output} #{items_output}"
     end
   end
 end
